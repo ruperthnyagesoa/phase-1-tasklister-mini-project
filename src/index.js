b465dbf0af70b7c2c7984487adf6545ed2b820de
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-22 12:17:00 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    handleTodo();
+    form.reset();
+   });
+
+  function handleTodo(todo){
+    todo = document.getElementById("new_task_description").value; 
+    let li = document.createElement("LI");
+    let BTN = document.createElement("button");
+    BTN.addEventListener("click", deleteToDo);
+    BTN.textContent = "delete"
+    li.textContent = `${todo}`;
+    let tasks = document.getElementById("tasks");
+    tasks.appendChild(li);
+    li.appendChild(BTN);
+   }
+   function deleteToDo(e){
+    e.target.parentNode.remove();
+   }
 });
